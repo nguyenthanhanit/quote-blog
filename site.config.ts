35665e42573cee824c385d967026f59bca90a493
An Nguyen <annt14@vng.com.vn>
2022-06-10 09:23:18 +0700

    Set rootNotionPageId

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: process.env.NOTION_PAGE_ID || '',
+  rootNotionPageId: '35e13bfc170d487a89e5fc1dd6f8bf98',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
