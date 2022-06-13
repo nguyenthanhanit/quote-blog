dfda0a6974ac282ae35b2dc2d5f1a7673bb16f84
An Nguyen <annt14@vng.com.vn>
2022-06-13 21:11:09 +0700

    Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,7 +33,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2022 {config.author}</div>
+      {/*<div className={styles.copyright}>Copyright 2022 {config.author}</div>*/}
 
       <div className={styles.settings}>
         {hasMounted && (
@@ -49,79 +49,79 @@ export const FooterImpl: React.FC = () => {
         )}
       </div>
 
-      <div className={styles.social}>
-        {config.twitter && (
-          <a
-            className={styles.twitter}
-            href={`https://twitter.com/${config.twitter}`}
-            title={`Twitter @${config.twitter}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaTwitter />
-          </a>
-        )}
+      {/*<div className={styles.social}>*/}
+      {/*  {config.twitter && (*/}
+      {/*    <a*/}
+      {/*      className={styles.twitter}*/}
+      {/*      href={`https://twitter.com/${config.twitter}`}*/}
+      {/*      title={`Twitter @${config.twitter}`}*/}
+      {/*      target='_blank'*/}
+      {/*      rel='noopener noreferrer'*/}
+      {/*    >*/}
+      {/*      <FaTwitter />*/}
+      {/*    </a>*/}
+      {/*  )}*/}
 
-        {config.zhihu && (
-          <a
-            className={styles.zhihu}
-            href={`https://zhihu.com/people/${config.zhihu}`}
-            title={`Zhihu @${config.zhihu}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaZhihu />
-          </a>
-        )}
+      {/*  {config.zhihu && (*/}
+      {/*    <a*/}
+      {/*      className={styles.zhihu}*/}
+      {/*      href={`https://zhihu.com/people/${config.zhihu}`}*/}
+      {/*      title={`Zhihu @${config.zhihu}`}*/}
+      {/*      target='_blank'*/}
+      {/*      rel='noopener noreferrer'*/}
+      {/*    >*/}
+      {/*      <FaZhihu />*/}
+      {/*    </a>*/}
+      {/*  )}*/}
 
-        {config.github && (
-          <a
-            className={styles.github}
-            href={`https://github.com/${config.github}`}
-            title={`GitHub @${config.github}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaGithub />
-          </a>
-        )}
+      {/*  {config.github && (*/}
+      {/*    <a*/}
+      {/*      className={styles.github}*/}
+      {/*      href={`https://github.com/${config.github}`}*/}
+      {/*      title={`GitHub @${config.github}`}*/}
+      {/*      target='_blank'*/}
+      {/*      rel='noopener noreferrer'*/}
+      {/*    >*/}
+      {/*      <FaGithub />*/}
+      {/*    </a>*/}
+      {/*  )}*/}
 
-        {config.linkedin && (
-          <a
-            className={styles.linkedin}
-            href={`https://www.linkedin.com/in/${config.linkedin}`}
-            title={`LinkedIn ${config.author}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaLinkedin />
-          </a>
-        )}
+      {/*  {config.linkedin && (*/}
+      {/*    <a*/}
+      {/*      className={styles.linkedin}*/}
+      {/*      href={`https://www.linkedin.com/in/${config.linkedin}`}*/}
+      {/*      title={`LinkedIn ${config.author}`}*/}
+      {/*      target='_blank'*/}
+      {/*      rel='noopener noreferrer'*/}
+      {/*    >*/}
+      {/*      <FaLinkedin />*/}
+      {/*    </a>*/}
+      {/*  )}*/}
 
-        {config.newsletter && (
-          <a
-            className={styles.newsletter}
-            href={`${config.newsletter}`}
-            title={`Newsletter ${config.author}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaEnvelopeOpenText />
-          </a>
-        )}
+      {/*  {config.newsletter && (*/}
+      {/*    <a*/}
+      {/*      className={styles.newsletter}*/}
+      {/*      href={`${config.newsletter}`}*/}
+      {/*      title={`Newsletter ${config.author}`}*/}
+      {/*      target='_blank'*/}
+      {/*      rel='noopener noreferrer'*/}
+      {/*    >*/}
+      {/*      <FaEnvelopeOpenText />*/}
+      {/*    </a>*/}
+      {/*  )}*/}
 
-        {config.youtube && (
-          <a
-            className={styles.youtube}
-            href={`https://www.youtube.com/${config.youtube}`}
-            title={`YouTube ${config.author}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaYoutube />
-          </a>
-        )}
-      </div>
+      {/*  {config.youtube && (*/}
+      {/*    <a*/}
+      {/*      className={styles.youtube}*/}
+      {/*      href={`https://www.youtube.com/${config.youtube}`}*/}
+      {/*      title={`YouTube ${config.author}`}*/}
+      {/*      target='_blank'*/}
+      {/*      rel='noopener noreferrer'*/}
+      {/*    >*/}
+      {/*      <FaYoutube />*/}
+      {/*    </a>*/}
+      {/*  )}*/}
+      {/*</div>*/}
     </footer>
   )
 }
